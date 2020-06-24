ed2ca9a2e36920e5ea5745d4b5bd9761a31bbb1e
raochoa019 <raochoa@espol.edu.ec>
2020-06-23 21:51:15 -0500

    Creacion de clase main y Pantalla en blanco

---
+++ Observer/src/Observer/Main.java
@@ -0,0 +1,20 @@
+package Observer;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class Main extends Application{
+	   @Override
+	    public void start(Stage stage) {
+	        Scene scene = new Scene (new StackPane(), 500, 500);
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+
+	    public static void main(String[] args) {
+	        launch();
+	    }
+
+}
